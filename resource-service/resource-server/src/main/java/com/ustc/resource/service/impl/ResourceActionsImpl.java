4f0dd0528ba08ff6ee4b70738af36ad01712557e
Gloic-amber <3534966216@qq.com>
2023-12-20 09:25:43 +0800

    使用了grpc来代替了resource-client中的openfeign的头像上传服务

--- resource-service/resource-server/src/main/java/com/ustc/resource/service/impl/ResourceActionsImpl.java
+++ resource-service/resource-server/src/main/java/com/ustc/resource/service/impl/ResourceActionsImpl.java
@@ -1,18 +1,23 @@
 package com.ustc.resource.service.impl;
 
-import com.ustc.common.grpc.ResourceActionsGrpc;
-import com.ustc.common.grpc.SchoolCode;
-import com.ustc.common.grpc.University;
+import com.ustc.common.grpc.*;
+import com.ustc.resource.service.ImageService;
 import com.ustc.resource.service.UniversityService;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.apache.http.entity.ContentType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * ClassName: ResourceActionsImpl
  * Package: com.ustc.resource.service.impl
- * Description:
+ * Description: grpc的服务端
  *
  * @Author Gloic
  * @Create 2023/12/19
@@ -25,10 +30,32 @@ public class ResourceActionsImpl extends ResourceActionsGrpc.ResourceActionsImpl
     @Resource
     private UniversityService universityService;
 
+    @Resource
+    private ImageService imageService;
+
+    // 获得大学名称
     @Override
     public void getUniversityName(SchoolCode request, StreamObserver<University> responseObserver){
         String universityName = universityService.getUniversityName(request.getCode());
         responseObserver.onNext(University.newBuilder().setName(universityName).build());
+        responseObserver.onCompleted();
+    }
+
+    // 上传头像
+    @Override
+    public void uploadAvatarImage(AvatarImage request, StreamObserver<ImageUrl> responseObserver){
+        // 将request中的字节数组转化为MultipartFile文件
+        InputStream inputStream = new ByteArrayInputStream(request.getImage().toByteArray());
+        MultipartFile image = null;
+        try {
+            image = new MockMultipartFile(request.getName(), request.getName(), request.getContentType(), inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // 调用图片服务进行上传
+        String url = imageService.uploadAvatar(image, request.getName());
+        // 传回URL参数
+        responseObserver.onNext(ImageUrl.newBuilder().setUrl(url).build());
         responseObserver.onCompleted();
     }
 }
--- user-service/user-server/src/main/java/com/ustc/user/service/impl/GrpcClientService.java
+++ user-service/user-server/src/main/java/com/ustc/user/service/impl/GrpcClientService.java
@@ -1,15 +1,17 @@
 package com.ustc.user.service.impl;
 
-import com.ustc.common.grpc.ResourceActionsGrpc;
-import com.ustc.common.grpc.SchoolCode;
-import com.ustc.common.grpc.University;
+import com.google.protobuf.ByteString;
+import com.ustc.common.grpc.*;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * ClassName: GrpcClientService
  * Package: com.ustc.user.service.impl
- * Description:
+ * Description: grpc的客户端
  *
  * @Author Gloic
  * @Create 2023/12/19
@@ -21,10 +23,30 @@ public class GrpcClientService {
     @GrpcClient("resource-server")
     ResourceActionsGrpc.ResourceActionsBlockingStub resourceActionsBlockingStub;
 
+    // 获得大学名称
     public String getUniversityName(int schoolCode){
         University university = resourceActionsBlockingStub.getUniversityName(
                 SchoolCode.newBuilder().setCode(schoolCode).build()
         );
         return university.getName();
     }
+
+    // 上传头像
+    public String uploadAvatarImage(MultipartFile file, String name){
+        // 将MultipartFile转化为需要传输的ImageUrl类型后，调用远程服务
+        ImageUrl imageUrl = null;
+        try {
+            imageUrl = resourceActionsBlockingStub.uploadAvatarImage(
+                    AvatarImage.newBuilder()
+                            .setImage(ByteString.copyFrom(file.getBytes()))
+                            .setName(name)
+                            .setContentType(file.getContentType())
+                            .build()
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return imageUrl.getUrl();
+    }
+
 }
